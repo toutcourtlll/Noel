9832e0754b61220e2486a63340596c9e0707f033
toutcourtlll <46397508+toutcourtlll@users.noreply.github.com>
2021-11-30 22:07:47 +0100

    Update sapinDeNoel.py

--- sapinDeNoel.py
+++ sapinDeNoel.py
@@ -22,59 +22,63 @@ while number_pass == False:
 #pl est le sapin
  
 while processing < int(lenght):
-    pl = ' ' + pl                    #ceci permet au code de mettre les espaces
-    processing += 1                  #tout en haut pour que le sapin soit droit
+min = 5
+max = 21
+number_pass = False
+processing = -1
+pl = '^'               # pl est une variable qui va aider a construire les branches
+
+ #number_pass est une boucle qui verifie si le nombre choisi correspond aux critères min et max
  
+ #lenght est une variable choisie par l'utilisateur qui va correspondre a l'échelle du sapin
+ 
+while number_pass == False:
+    lenght =  input("Choisissez une taille comprise entre " + str(min) + " et " + str(max) + " ")
+    if int(lenght) < min:
+        print('le nombre choisis est trop petit !')
+    elif int(lenght) > max:
+        print('le nombre choisis est trop grand !')
+    else:
+        number_pass = True     #passe numbre_pass sur false se qui arrete la boucle
+ 
+for i in range(int(lenght) + 1):
+    pl = ' ' + pl                    #ceci permet au code de mettre les espaces
+                                     #tout en haut pour que le sapin soit droit
 print(pl)
 
-while a <= int(lenght):
+for i in range(int(lenght) + 1):
  
     pl = pl[1:len(pl)]
-    a += 1
-    pl = pl + '^^'
+    pl += '^^'
     print(pl)
- 
-a = 0
- 
+  
  # # # # # # # # # # # # # # # #
  #on passe a la parti du tronc #
- 
- 
  
 log = ' '                                #log = tronc
 logl = round(int(lenght) / 2, 0)         #logl = largeur du tronc
 plenM = len(pl) / 2 + 0.5                #plenM est le centre du sapin
  
- #on calcule ici si logl est pair ou unpair et si est sur pair le met sur unpair
+ #on calcule ici si logl est pair ou unpair et si est sur pair ajoute 1
 if (logl % 2) == 0:
    logl += 1
 while len(log) < plenM:
     log = str(log) + ' '
+    
 boucle = (logl-1)/2
 
-
-
-# on retire ici la moitié -1 du centre afin de ne pas avoir le teonc de decalé
-while a <= boucle:
+for i in range(int(boucle) + 1):
     log = log[1:len(log)]
-    a += 1
     
-a = 0
-
-
 # on créer ici la taille du tronc
-while a < logl:
+for i in range(int(logl)):
     log = log + 'I'
-    a += 1
- 
+    
  #la variable hight est la hauteur du tronc
 hight = round(len(log.replace('  ','')))/2 + 0.5
 #                 ^^^^^^^^^^^^^^^^^^^^
 #                 retire tout les espaces necessaire au cadrage du tronc du  calcul
- 
-a = 0
 
 #met le tronc dans la console
-while a < hight:
+for i in range(int(hight) + 1):
     print(log)
-    a += 1
